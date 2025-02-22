c34ab643cb92df9596ee0976375768227bb4c10b
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-02-23 00:45:45 +0300

    настроена минимальная функциональность парсера

--- app_parser/main.py
+++ app_parser/main.py
@@ -1,8 +1,9 @@
 import re
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 
-from constants import MAIN_URL, YEAR_PATTERN
-from utils import get_soup
+from constants import DAY_PATTERN, DAY_INDEX_PATTERN, MAIN_URL, YEAR_PATTERN
+from utils import get_days_tag, get_month_table, get_soup
 
 
 def get_year(soup: BeautifulSoup) -> int:
@@ -10,11 +11,26 @@ def get_year(soup: BeautifulSoup) -> int:
     return int(re.search(YEAR_PATTERN, soup.find('h1').text).group('year'))
 
 
+def get_month(month: Tag) -> str:
+    """Возвращает месяц в виде строки."""
+    return month.find('th', {'class': 'month'}).text
+
+
+def get_day(day: Tag) -> int:
+    """Возвращает число месяца."""
+    return int(re.search(DAY_INDEX_PATTERN, day.text).group('day'))
+
+
 def main():
     """Главная функция парсера."""
     soup = get_soup(MAIN_URL)
+    month_table = get_month_table(soup)
     year = get_year(soup)
-    print(year)
+    for month in month_table:
+        month_title = get_month(month)
+        for day in get_days_tag(month):
+            if DAY_PATTERN.match(day.text):
+                print((get_day(day), month_title, year))
 
 
 if __name__ == '__main__':
--- app_parser/utils.py
+++ app_parser/utils.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 
 def get_soup(url: str, parser: str = 'lxml') -> BeautifulSoup:
@@ -8,3 +9,13 @@ def get_soup(url: str, parser: str = 'lxml') -> BeautifulSoup:
     возвращает HTML страницы в виде текста.
     """
     return BeautifulSoup(requests.Session().get(url).text, parser)
+
+
+def get_month_table(soup: BeautifulSoup) -> list:
+    """Возвращает список тэгов календаря для каждого месяца."""
+    return soup.find_all('table', {'class': 'cal'})
+
+
+def get_days_tag(month: Tag) -> list:
+    """Возвращает список тэгов дней в месяце."""
+    return month.find_all('td')
