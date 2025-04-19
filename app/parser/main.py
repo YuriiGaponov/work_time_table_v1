eea0eef1c48307df7c45a0fd019e0c5132f6f3a1
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-04-20 02:05:53 +0300

    константы парсера убраны в класс

--- app/parser/main.py
+++ app/parser/main.py
@@ -2,19 +2,15 @@ import re
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
-from .constants import (
-    DAY_PATTERN,
-    DAY_INDEX_PATTERN,
-    MAIN_URL,
-    YEAR_PATTERN,
-    WEEKEND_PATTERN
-)
+from .constants import parser_constants
 from .utils import get_days_tag, get_month_table, get_soup
 
 
 def get_year(soup: BeautifulSoup) -> int:
     """Возвращает год календаря в виде числа."""
-    return int(re.search(YEAR_PATTERN, soup.find('h1').text).group('year'))
+    return int(re.search(
+        parser_constants.YEAR_PATTERN, soup.find('h1').text
+    ).group('year'))
 
 
 def get_month(month: Tag) -> str:
@@ -24,29 +20,28 @@ def get_month(month: Tag) -> str:
 
 def get_day(day: Tag) -> int:
     """Возвращает число месяца."""
-    return int(re.search(DAY_INDEX_PATTERN, day.text).group('day'))
+    return int(re.search(
+        parser_constants.DAY_INDEX_PATTERN, day.text
+    ).group('day'))
 
 
 def is_weekend(day: Tag) -> bool:
-    """
-    Возращает "выходной", если день является выходным или праздничным,
-    в остальных случаях возвращает "рабочий".
-    """
-    return 'выходной' if WEEKEND_PATTERN in day['class'] else 'рабочий'
+    """Возращает True, если день является выходным или праздничным."""
+    return parser_constants.WEEKEND_PATTERN in day['class']
 
 
 def main():
     """Главная функция парсера."""
-    soup = get_soup(MAIN_URL)
+    soup = get_soup(parser_constants.MAIN_URL)
     year = get_year(soup)
     day_count = 0
     for month in get_month_table(soup):
         month_title = get_month(month)
         for day in get_days_tag(month):
-            if DAY_PATTERN.match(day.text):
+            if parser_constants.DAY_PATTERN.match(day.text):
                 day_count += 1
                 day_nuber = get_day(day)
-                weekend = is_weekend(day)
+                weekend = 'выходной' if is_weekend(day) else 'рабочий'
                 print((day_count, day_nuber, month_title, year, weekend))
 
 
