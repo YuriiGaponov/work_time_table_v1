7e4af903ee92b5ff9f2418f931977f4076107b90
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-05-08 14:51:17 +0300

    переопределена модель CalendarDay

--- app/calendar/models.py
+++ app/calendar/models.py
@@ -1,7 +1,11 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Boolean, Column, Integer, String
 
 from ..db import Base
 
 
 class CalendarDay(Base):
     number: int = Column(Integer)
+    day: int = Column(Integer)
+    month: str = Column(String)
+    year: int = Column(Integer)
+    is_weekend: bool = Column(Boolean)
