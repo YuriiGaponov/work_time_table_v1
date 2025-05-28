e3eb68a2ae2187467ef6d9811f93a759ffed1f59
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-05-29 01:14:00 +0300

    поправлен __init__.py

--- app/parser/__init__.py
+++ app/parser/__init__.py
@@ -1,1 +1,8 @@
-from .main import main as parser # noqa
+# Инициализация модуля, экспорт основного парсера
+from .main import main as parser
+
+# Экспортируемые имена
+__all__ = ['parser']
+
+if __name__ == '__main__':
+    parser()
