1964164ff3f0b28bdce77f34a8647ccea6e33fc1
YuriiGaponov <GREATLORDorel@yandex.ru>
2025-05-30 07:37:56 +0300

    убрано двойное получение супа в парсере, рефакторинг названий методов парсера

--- app/parser/parser.py
+++ app/parser/parser.py
@@ -1,7 +1,7 @@
 import re
 import requests
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 from bs4 import BeautifulSoup
 from bs4.element import Tag
@@ -17,42 +17,45 @@ class ConsultantPlusParser():
     """
 
     config: ConsultantPlusParserConfig
+    _soup: Optional[BeautifulSoup] = None
 
-    def get_soup(self, parser: str = 'lxml') -> BeautifulSoup:
+    def _get_soup(self, parser: str = 'lxml') -> BeautifulSoup:
         """
         Принимает на вход url страницы и парсер
         (по умолчанию используется lxml),
         возвращает HTML страницы в виде текста.
         """
-        return BeautifulSoup(
-            requests.Session().get(self.config.MAIN_URL).text, parser
-        )
+        if self._soup is None:
+            self._soup = BeautifulSoup(
+                requests.Session().get(self.config.MAIN_URL).text, parser
+            )
+        return self._soup
 
-    def get_year(self) -> int:
+    def _get_year(self) -> int:
         """Возвращает год календаря в виде числа."""
         return int(re.search(
-            self.config.YEAR_PATTERN, self.get_soup().find('h1').text
+            self.config.YEAR_PATTERN, self._get_soup().find('h1').text
         ).group('year'))
 
-    def get_month_table(self) -> List[Tag]:
+    def _get_month_table(self) -> List[Tag]:
         """Возвращает список тэгов календаря для каждого месяца."""
-        return self.get_soup().find_all('table', {'class': 'cal'})
+        return self._get_soup().find_all('table', {'class': 'cal'})
 
-    def get_month_title(self, month: Tag) -> str:
+    def _get_month_title(self, month: Tag) -> str:
         """Возвращает название месяца в виде строки."""
         return month.find('th', {'class': 'month'}).text
 
-    def get_days_tag(self, month: Tag) -> List[Tag]:
+    def _get_days_tag(self, month: Tag) -> List[Tag]:
         """Возвращает список тэгов дней в месяце."""
         return month.find_all('td')
 
-    def get_day(self, day: Tag) -> int:
+    def _get_day(self, day: Tag) -> int:
         """Возвращает число месяца."""
         return int(re.search(
             self.config.DAY_INDEX_PATTERN, day.text
         ).group('day'))
 
-    def is_weekend(self, day: Tag) -> bool:
+    def _is_weekend(self, day: Tag) -> bool:
         """Возращает True, если день является выходным или праздничным."""
         return self.config.WEEKEND_PATTERN in day['class']
 
@@ -62,19 +65,19 @@ class ConsultantPlusParser():
         номер дня в году, число, название месяца, год, является ли выходным.
         """
         result = []
-        year = self.get_year()
+        year = self._get_year()
         day_count = 0
-        for month in self.get_month_table():
-            month_title = self.get_month_title(month)
-            for day in self.get_days_tag(month):
+        for month in self._get_month_table():
+            month_title = self._get_month_title(month)
+            for day in self._get_days_tag(month):
                 if self.config.DAY_PATTERN.match(day.text):
                     day_count += 1
                     result.append(
                         (
                             day_count,
-                            self.get_day(day),
+                            self._get_day(day),
                             month_title, year,
-                            self.is_weekend(day)
+                            self._is_weekend(day)
                         )
                     )
         return result
