6b9367699610156346b0d4987913aecaf7da609a
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-07-15 15:15:26 +0300

    создан базовый шаблон окна

---
+++ app/gui/views/main.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+
+class MainView():
+    """Главное окно приложения"""
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from .calendar import save_calendar
 from .db import Base, engine, session
+from .gui import MainView
 from .parser import parse_calendar, parser
 
 
 def main() -> None:
     """Главная функция приложения."""
-    return save_calendar(session, parse_calendar(parser))
+    app = MainView()
+    app.run()
+    # return save_calendar(session, parse_calendar(parser))
 
 
 if __name__ == '__main__':
