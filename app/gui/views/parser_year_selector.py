a2835af695ebc8bdea5560cd70e25ea2425ecfc5
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-07-18 02:11:16 +0300

    настроена кнопка очистки поля ввода года

--- app/gui/views/parser_year_selector.py
+++ app/gui/views/parser_year_selector.py
@@ -21,11 +21,12 @@ class ParserYearSelector(BaseView):
         self.confirm_button = tk.Button(
             self.root_content_frame,
             text=Config.CONFIRM_BUTTON_TEXT,
-            command=self.get_year
+            command=self.confirm
         )
         self.clean_button = tk.Button(
             self.root_content_frame,
-            text=Config.CLEAN_BUTTON_TEXT
+            text=Config.CLEAN_BUTTON_TEXT,
+            command=self.clean
         )
 
         self.select_year_lable.grid(row=0, column=0, sticky=Config.STICKY)
@@ -33,7 +34,11 @@ class ParserYearSelector(BaseView):
         self.confirm_button.grid(row=1, column=0, sticky=Config.STICKY)
         self.clean_button.grid(row=1, column=1, sticky=Config.STICKY)
 
-    def get_year(self):
+    def confirm(self):
         """Получает год из поля ввода и передает его в парсер."""
         get_calendar(self.select_year_entry.get())
         self.root.destroy()
+
+    def clean(self):
+        """Очищает поле ввода года."""
+        self.select_year_entry.delete(0, tk.END)
