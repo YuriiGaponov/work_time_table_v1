cb61ba34b237d5355c6a2ec13ec4166618072c0c
YuriiGaponov <147429527+YuriiGaponov@users.noreply.github.com>
2025-07-19 15:50:49 +0300

    вызываемые окна сделаны модальными (#8)

--- app/gui/views/main.py
+++ app/gui/views/main.py
@@ -17,6 +17,11 @@ class MainView(BaseView):
         self.get_calendar_button = tk.Button(
             self.root_content_frame,
             text=Config.GET_CALENDAR_BUTTON_TEXT,
-            command=open_year_selector
+            # command=open_year_selector
+            command=self.year_selector
         )
         self.get_calendar_button.grid(row=0, column=0, sticky=Config.STICKY)
+
+    def year_selector(self):
+        """Открыть окно ввода года."""
+        open_year_selector(self)
---
+++ app/gui/views/parser_views/__init__.py
@@ -0,0 +1,4 @@
+from .incorrect_year import IncorrectYearView
+from .year_selector import YearSelectorView
+
+__all__ = ['IncorrectYearView', 'YearSelectorView']
