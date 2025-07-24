ac2c2bc0f97c588e0fba1a6eb8060c1ac38361ce
YuriiGaponov <GREATLORDorel@yandex.ru>
2025-07-24 14:42:25 +0300

    создана модель сотрудника

---
+++ app/employees/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, String, UniqueConstraint
+from app.db import Base
+
+
+class Employee(Base):
+    """Класс для хранения сведений о сотруднике."""
+
+    name: str = Column(String)
+    patronymic: str = Column(String, nullable=True)
+    surname: str = Column(String)
+    department: str = Column(String)
+
+    __table_args__ = (
+        UniqueConstraint(
+            'name', 'patronymic', 'surname', 'department',
+            name='ux_employee'
+        )
+    )
+
+    def __init__(self, name: str, surname: str, department: str,
+                 patronymic: str = None):
+        """Конструктор для создания нового сотрудника"""
+        self.name = name
+        self.surname = surname
+        self.department = department
+        self.patronymic = patronymic
