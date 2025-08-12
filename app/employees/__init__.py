2831f6e3c005b2a23068cd1e1a5810997fffad74
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-08-12 04:43:22 +0300

    создана функция поиска сотрудников

--- app/employees/__init__.py
+++ app/employees/__init__.py
@@ -1,4 +1,4 @@
-from .main import save_employee
+from .main import save_employee, search_employees
 from .schemas import EmployeeSchema
 from .validators import EmployeeValidator, employee_exist, validate_employee
 
@@ -7,5 +7,6 @@ __all__ = [
     'EmployeeSchema',
     'EmployeeValidator',
     'save_employee',
+    'search_employees',
     'validate_employee'
 ]
--- app/employees/main.py
+++ app/employees/main.py
@@ -1,6 +1,41 @@
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
+from typing import Optional, List
+
 from .models import Employee
 from .schemas import EmployeeSchema
+
+
+def search_employees(
+    session: Session,
+    name: Optional[str] = None,
+    patronymic: Optional[str] = None,
+    surname: Optional[str] = None,
+    department: Optional[str] = None
+) -> List[Employee]:
+    """
+    Поиск сотрудников по одному или нескольким полям.
+    """
+    query = session.query(Employee)
+
+    filters = []
+
+    if name:
+        filters.append(Employee.name.ilike(f'%{name}%'))
+
+    if patronymic:
+        filters.append(Employee.patronymic.ilike(f'%{patronymic}%'))
+
+    if surname:
+        filters.append(Employee.surname.ilike(f'%{surname}%'))
+
+    if department:
+        filters.append(Employee.department == department)
+
+    if filters:
+        query = query.filter(and_(*filters))
+
+    return query.all()
 
 
 def save_employee(session: Session, data: EmployeeSchema):
